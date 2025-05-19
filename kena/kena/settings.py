a45ba57c0ad5a82f6e7daf95047b19971eba8b10
Harrison-Muraya <harrisonmuraya8@gmail.com>
2025-05-19 16:32:18 +0300

    Update database settings to include password

--- kena/kena/settings.py
+++ kena/kena/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'coin',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
