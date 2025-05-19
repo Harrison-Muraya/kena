5931a4922e9b1876a06cb5327041e6077628aa19
Harrison-Muraya <harrisonmuraya8@gmail.com>
2025-05-19 17:27:50 +0300

    Updated views and URLs for the coin app: changed index view to home view, updated URLs to reflect the change.

--- kena/coin/views.py
+++ kena/coin/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
-def index(request):
-    return HttpResponse('hello world')
+
+def home(request):
+    return render(request, 'coin/home.html')
