579321310cafe87577a5b07eba09c607d3703e39
Harrison-Muraya <harrisonmuraya8@gmail.com>
2025-07-27 04:53:44 +0300

    Added admin site registrations for Todolist, Item, Coin, and Wallet models.

--- kena/coin/admin.py
+++ kena/coin/admin.py
@@ -3,3 +3,7 @@ from .models import CustomUser, Todolist, Item, Coin, Wallet
 
 # Register your models here.
 admin.site.register(CustomUser)
+admin.site.register(Todolist)
+admin.site.register(Item)
+admin.site.register(Coin)
+admin.site.register(Wallet)
