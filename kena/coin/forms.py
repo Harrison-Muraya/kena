2a5f7b09457108387c1c4221686407b898294180
Harrison-Muraya <harrisonmuraya8@gmail.com>
2025-12-09 02:18:09 +0300

    `Updated wallet display in dashboard with wallet address copy functionality and balance display`

--- kena/coin/forms.py
+++ kena/coin/forms.py
@@ -213,26 +213,28 @@ class RegisterForm(UserCreationForm):
             )
 
             # Create a default wallet for the user
-            # default_coin = Coin.objects.first()  # Assuming there's at least one coin
-            # data = {
-            #     'name': 'Default Wallet',
-            #     'password': self.cleaned_data.get('password1'),
-            #     'wallettype': 'primary',
-            #     'private_key': views.checkKey(user.private_key).export_key().decode() #if user.private_key else ""
-            # }
-            # hasher = blockchain.CalculateHash(data)
-            # hash_value = hasher.calculate()
-            # # You can also create a default wallet here if needed
-            # Wallet.objects.create(
-            #     user=user,
-            #     coin=default_coin,
-            #     name="Default Wallet",
-            #     amount=0,
-            #     value=0,
-            #     hash=hash_value,
-            #     password=make_password(self.cleaned_data.get('password1')),  # hashed like in dashboard
-            #     wallettype="primary",
-            # )
+            default_coin = Coin.objects.first()  # Assuming there's at least one coin
+            data = {
+                'name': 'Default Wallet',
+                # 'password': self.cleaned_data.get('password1'),
+                'password': '12345678',  # Empty password for hash calculation
+                'wallettype': 'primary',
+                'private_key': views.checkKey(user.private_key).export_key().decode() #if user.private_key else ""
+            }
+            hasher = blockchain.CalculateHash(data)
+            hash_value = hasher.calculate()
+            # You can also create a default wallet here if needed
+            Wallet.objects.create(
+                user=user,
+                coin=default_coin,
+                name="Default Wallet",
+                amount=0,
+                value=0,
+                hash=hash_value,
+                # password=make_password(self.cleaned_data.get('password1')),  # hashed like in dashboard
+                password=make_password('12345678'),  # hashed like in dashboard
+                wallettype="primary",
+            )
 
         
         return user
