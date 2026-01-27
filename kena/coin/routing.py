d2d2b7853f64f4d4135997a16dd06af00e1e1415
Harrison-Muraya <112485035+Harrison-Muraya@users.noreply.github.com>
2026-01-27 13:36:11 +0300

    Delete kena directory

--- kena/coin/routing.py
+++
@@ -1,9 +0,0 @@
-from django.urls import re_path
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(r'ws/test/$', consumers.TestConsumer.as_asgi()),
-    re_path(r'ws/transactions/$', consumers.TransactionConsumer.as_asgi()),
-    re_path(r'ws/blocks/$', consumers.BlockConsumer.as_asgi()),
-    # re_path(r'ws/mpesa_status/$', consumers.MpesaStatusConsumer.as_asgi()),
-]
