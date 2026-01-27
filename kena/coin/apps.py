6eeb66541eeafdc584509d8f79117eb1b6f670fe
Harrison-Muraya <112485035+Harrison-Muraya@users.noreply.github.com>
2026-01-27 14:24:53 +0300

    Delete kena directory

--- kena/coin/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class CoinConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'coin'
-
-    def ready(self):
-        import coin.signals
